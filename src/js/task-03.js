f3d1da16824714f0ad81a30e2ba2f972c2d799d3
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-06-03 19:07:41 +0300

    work

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = images
+  .map(({ url, alt }) => `<li><img src="${url}" alt="${alt}"></li>`)
+  .join('');
+
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
